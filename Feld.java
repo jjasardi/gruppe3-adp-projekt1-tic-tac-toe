4c67e0111215e3e03774daa87d0444698567943c
Durim Sadiku <sadikdur@students.zhaw.ch>
2021-10-05 21:16:26 +0200

    fast plausible methode

--- Feld.java
+++ Feld.java
@@ -2,29 +2,22 @@
 
 public class Feld
 {
-    int x1;
-    int x2;
-    int x3;
-    int y1;
-    int y2;
-    int y3;
-    int z1;
-    int z2;
-    int z3;
+    int wert;
     boolean spielerZug;
     
-    public Feld()
+    public Feld(int wert)
     {
-        }
+        wert=0;
+    }
 
 
-    public void x1()
+    public void feldSetzen()
     {
         if (spielerZug == false){
-            x1 = 1;
+            wert = 1;
         }    else {
-                x1 = 2;
+                wert = 2;
             }
          spielerZug=(!spielerZug);
-    }
+    } 
 }
--- Spielfeld.java
+++ Spielfeld.java
@@ -13,34 +13,19 @@ public class Spielfeld
     
     public Spielfeld(){
         spielerZug=false;
-        x1=new Feld();
-        x3=new Feld(0,2);
-        y1=new Feld(1,0);
-        y2=new Feld(1,1);
-        y3=new Feld(1,2);
-        z1=new Feld(2,0);
-        z2=new Feld(2,1);
-        z3=new Feld(2,2);
-    }
+        x1= new Feld(0);
+        x2= new Feld(0);
+        }
    
-    public void setFeld(){
-        
+    public void x1(){
+        x1.feldSetzen();
+        spielerZug=(!spielerZug);
     }
     
     public void getFeld(){
     
     }
-    
-    public void feldSetzen(int feldGesetzt)
-    {   
-        if (spielerZug == false){
-            feldGesetzt= 1;
-        }    else {
-                feldGesetzt= 2;
-            }
-         spielerZug=(!spielerZug);
-        }
-    
+        
     public void druck() {   
         System.out.println(x1+", "+x2+", "+x3);
         System.out.println(y1+", "+y2+", "+y3);
